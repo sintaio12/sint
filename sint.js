584dec9e659ada9b1452e7c07729c811fc4478ce
sintaio12 <a68499685@gmail.com>
2023-02-02 00:01:53 -0800

    added prime function

---
+++ sint.js
@@ -0,0 +1,13 @@
+function isprime(userinput) {
+    if (userinput == 1) {
+       return false; 
+    }
+   else if (userinput == 2) 
+   {
+    return this;
+        
+    }
+    var nsqrt = math.sqrt(userinput);
+for (i = 2 ); i <nserinput>s 
+
+}
